985bfaf0310f5eb73e8062a6144a995700ecb040
ushieru <uzielcocolan@gmail.com>
2022-03-06 14:27:17 -0600

    add api rest example

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from flask import Flask
+import json
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+users = []
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+
+@app.route("/", methods=['GET'])
+def get_users():
+    return jsonify(users)
+
+
+@app.route("/", methods=['POST'])
+def post_users():
+    body = json.loads(request.data)
+    new_user= {
+        "name": body['name'],
+        "lastname": body['lastname'],
+        "email": body['email']
+    }
+    users.append(new_user)
+    return jsonify(new_user)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
